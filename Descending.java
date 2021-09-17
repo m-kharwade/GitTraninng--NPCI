00599850e08518eb2fa8e4c82b8fb13f9ce1543e
m-kharwade <47314052+m-kharwade@users.noreply.github.com>
2021-09-17 10:42:17 +0530

    Add files via upload

---
+++ Descending.java
@@ -0,0 +1,32 @@
+package com.npci.dsalgo.main;
+
+public class Descending {
+public static void main(String[] args) {
+		
+		int[] arr= new int[] {11,67,89,123,6,12,34};
+		int temp=0;
+		
+		for(int i=0;i<=arr.length-1;i++)
+		{
+			for(int j=i+1;j<=arr.length-1;j++)
+			{
+				if(arr[i]<arr[j])
+				{
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+					
+				}
+			}
+			
+		}
+		
+		for(int i=0;i<=arr.length-1;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+
+}
