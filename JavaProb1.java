5e22a1579108d82007bef64da4c5ebc069a6ddac
unknown <meenakshikharwade862@gmail.com>
2021-09-21 12:33:26 +0530

    Conditional Statements Assignment 1

--- JavaProb1.java
+++ JavaProb1.java
@@ -1,6 +1,3 @@
-//WAP to identify if a value is even & square or multiple of previous value
-
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class JavaProb1 {
@@ -17,13 +14,14 @@ public class JavaProb1 {
 
         if(firstValue % 2==0)
         {
-            if(firstValue * firstValue==secondValue )
-            {
-                if(secondValue % firstValue==0)
-                    System.out.println(secondValue +" is a multiple of " +firstValue);
+            if(firstValue * firstValue==secondValue && secondValue % firstValue==0)
+                System.out.println(secondValue +" is square and multiple of " +firstValue);
+            else if(firstValue * firstValue==secondValue)
                 System.out.println(secondValue+" is a square of " +firstValue);
-            }
-            System.out.println(secondValue +" is neither square nor multiple of " +firstValue);
+             else if(secondValue % firstValue==0)
+                System.out.println(secondValue +" is a multiple of " +firstValue);
+             else
+                System.out.println(secondValue +" is neither square nor multiple of " +firstValue);
         }
         else
         {
