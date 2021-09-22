f5ba23a6e8794f6731255b89006b7f6e93434997
unknown <meenakshikharwade862@gmail.com>
2021-09-22 10:34:07 +0530

    2d Matrix

---
+++ JavaProblem6.java
@@ -0,0 +1,20 @@
+
+public class JavaProblem6 {
+    public static void main(String[] args) {
+
+        int [][] arr={{1,2,3},{6,5,4}};
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr[i].length;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+}
