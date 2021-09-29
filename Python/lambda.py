8d97fb4c5379bbfd44ffa33b12b807f7e3a42b7b
unknown <meenakshikharwade862@gmail.com>
2021-09-29 12:54:39 +0530

    Lambda & ATM transaction

--- Python/lambda.py
+++ Python/lambda.py
@@ -1,7 +1,7 @@
 
 lst=[]
 dict1={}
-dict2={}
+
 
 def main():
     print("1.Add a new element to the list")
@@ -20,36 +20,43 @@ def main():
     def createDict():
         # dict1 = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)}
         # print(dict1)
+        global dict1
         dict1={n:n+n for n in lst}
         print(dict1)
         main()
-        return dict1
 
     def sortDict():
-        for i in range(len(dict1)):
-            sml=[a for a in dict1.values() if a ==min(dict1.values())][0]
-            key1=[k for k,v in dict1.items() if v ==min(dict1.values())][0]
-            del dict1[key1]
-            print(sml,key1,dict1[0],dict1[1])
-            # list1.append(sml)
-            # dict1[0]=dict1.keys
-            # dict1[1]=dict1.values
+        # global dict1
+    
+        # for i in range(len(dict1)):
+        #     sml=[a for a in dict1.values() if a ==min(dict1.values())][0]
+        #     key1=[k for k,v in dict1.items() if v ==min(dict1.values())][0]
+        #     del dict1[key1]
+        #     print(sml,key1,dict1[0],dict1[1])
+        #     # list1.append(sml)
+        #     # dict1[0]=dict1.keys
+        #     # dict1[1]=dict1.values
         # print(dict1)
+
+        sorted_dict=dict(sorted(dict1.items(),key=lambda item:item[1]))
+        print("Sorted Dict :",sorted_dict)
 
     if action==1:
         addElements()
         main()
     elif action==2:
         createDict()
+
     elif action==3:
-        sortDict()  
+        # global dict1
+        # print(dict1)
+        # sortDict(dict1)  
+        sortDict()
         main()         
     else:
         print("Enter valid option !")
         main()
 
 
-   
+ 
 main()       
-
-# add_lst=lambda 1
