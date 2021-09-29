5e9bfef72e5aca4834d5606eafc88128db0d2c93
unknown <meenakshikharwade862@gmail.com>
2021-09-29 17:47:42 +0530

    File IO

--- Python/FileOperation.py
+++ Python/FileOperation.py
@@ -1,48 +1,38 @@
 from abc import abstractproperty
 
 
-lst=['Name','Email','Phone','Meenakshi','m@gmail.com','1234','Mitali','mm@gmail.com,1234','Meenakshi']
+lst=['Name','Email','Phone','Meenakshi','m@gmail.com','1234','Mitali','mm@gmail.com,1234','Meenakshi','Mitali']
 lst1=[]
 lst_set=list(set(lst))
 lst_tuple=()
 
-print(lst)
-print(lst_set)
-count=0
+def f7(lst):
+    global seen
+    global seen_add
+    seen = set()
+    seen_add = seen.add
+    return [x for x in lst if not (x in seen or seen_add(x))]
+
+
+lst=f7(lst)
+
+
 file=open("sample.txt",'w')
 for i in lst: 
     if lst.index(i)==0:
         pass
     elif(lst.index(i)%3==0):
          file.write('\n')
-    for j in range(lst.index(i)+1,len(lst)):
-        if(i==lst[j]):
-            # print("Duplicate record")
-            count=count+1
-    if(count>1):
-        pass
-    else:
-        file.write(i)
-        file.write(',')
+    file.write(i)
+    file.write(',')
 
 
 
 with open('sample.txt','r') as file:
-    # print(file.read())
     Lines = file.readlines()
  
     count = 0
     for line in Lines:
-        # count += 1
-        # print("Line{}: {}".format(count, line.strip()))
-        # if '\n' in line:
-        #     for i in line:
-        #         if i=='\n':
-        #             continue
-        #         else:
-        #             lst1.append(line)
-        # else:
-        #     lst1.append(line)
         for word in line.split():
             lst1.append(word)
     lst_tuple=tuple(lst1)
